a969163dd092fa23fcf600f3045327d4e6d54492
Ruan Eloy <ruaneloy@pop-os.localdomain>
2018-11-16 20:41:45 -0300

    Implemented basic table functions

--- src/Table.hs
+++ src/Table.hs
@@ -1,13 +1,39 @@
 import qualified Data.Map as M
+import Data.Maybe (fromMaybe)
 
 
--- insert k v M.empty = M.insert k v M.empty 
-insert key value table = M.insert key [value] table
+{- 
+		Get the value associated with a key, 
+		if there is no value associated with the key
+		then it return an empty list 
+-}
+get key table = fromMaybe [] value
+    where value = search key table
 
+{-
+		Inserts a new value to the table,
+		if the key already exists then its associated value is updated
+-}
+insert key value table = M.insertWith (++) key [value] table
 
+{-
+    Verifies if the pair key value exists
+-}
+contain key value table = elem value (get key table) 
+
+{-
+    If the key is present it return Just the value,
+    otherwise Nothing is returned
+-}
 search key table = M.lookup key table
 
+{-
+    Removes all values associated with the given key
+-}
+removeAll key table = M.delete key table
 
-remove key table = M.delete key table
-
+{-
+    Remove the given value associate with the given key
+-}
+remove key value table = M.adjust (filter (\v -> v /= value)) key table
 
